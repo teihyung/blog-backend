8d5f26171dde9dcccbf3eebd9af2015cde178d9b
teihyung <teihyung@gmail.com>
2025-01-04 00:19:34 -0800

    i changed the endpoint of main page to return me hello tyepscript nodejs server.

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server is running");
+  res.send("Hello typescript nodejs server!");
 });
 
 app.listen(port, () => {
